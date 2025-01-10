3d6a532f7f3d500637f666a7a83bef842d081d9e
Johannes Förstner <johannes@famfoerstner.de>
2025-01-10 20:56:21 +0100

    Enable running build without GasPy

--- build.bat
+++ build.bat
@@ -21,30 +21,36 @@ set mode=%1
 echo %mode%
 
 :: pre-build checks
-pushd %gaspy%
 setlocal EnableDelayedExpansion
-if not "%mode%"=="light" (
-  set checks=standard
-  if "%mode%"=="release" (
-    set checks=all
+if not "%gaspy%"=="" (
+  if not "%mode%"=="light" (
+    pushd %gaspy%
+    set checks=standard
+    if "%mode%"=="release" (
+      set checks=all
+    )
+    venv\Scripts\python -m build.pre_build_checks %map% --check !checks! --bits "%bits%"
+    if !errorlevel! neq 0 pause
+    popd
   )
-  venv\Scripts\python -m build.pre_build_checks %map% --check !checks! --bits "%bits%"
-  if !errorlevel! neq 0 pause
 )
 endlocal
-popd
 
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%bits%\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /S
-pushd %gaspy%
-set dev_only=--dev-only-false
-if "%mode%"=="release" (
-  set dev_only=
+setlocal EnableDelayedExpansion
+if not "%gaspy%"=="" (
+  pushd %gaspy%
+  set dev_only=--dev-only-false
+  if "%mode%"=="release" (
+    set dev_only=
+  )
+  venv\Scripts\python -m build.fix_start_positions_required_levels %map% !dev_only! --bits "%tmp%\Bits"
+  if !errorlevel! neq 0 pause
+  popd
 )
-venv\Scripts\python -m build.fix_start_positions_required_levels %map% %dev_only% --bits "%tmp%\Bits"
-if %errorlevel% neq 0 pause
-popd
+endlocal
 "%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsmap" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
 
